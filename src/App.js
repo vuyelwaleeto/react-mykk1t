ba733a065c8f5f866035daeb1ce140278ee7889f
vuyelwaleeto <lindelwamaka99@gmail.com>
2021-07-08 23:23:08 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ const useStyles = makeStyles({
   header: {
     background: 'burlywood',
     textAlign: 'center',
-    fontSize: 30,
-    padding: 20
+    fontSize: 20,
+    paddingTop: 40,
+    paddingBottom: 80,
+    paddingLeft: 20,
+    fontWeight: 'bold'
   },
   profile: {
     background: 'white',
@@ -22,7 +25,7 @@ const useStyles = makeStyles({
     background: 'blanchedalmond',
     fontWeight: 'bold',
     padding: 20,
-    fontSize: 20
+    fontSize: 20,
   }
 });
 
@@ -30,65 +33,23 @@ export default function App() {
   const classes = useStyles();
 
   return (
-    <div>
-      <Grid container spacing={3} className={classes.grid}>
+    <div className={classes.root}>
+      <Grid container spacing={4} className={classes.grid}>
         <Grid item xs={12}>
           <Paper className={classes.header}>
-            VUYELWA MAKABA
-            <br />
-            <subtitle1> INFORMATION TECHNOLOGY GRADUATE </subtitle1>
-          </Paper>
-        </Grid>
-        <Grid item xs={9}>
-          <Paper className={classes.profile}>
-            PROFESSIONAL PROFILE
-            <br />
-            <br />
-            <subtitle1>
-              Having recently completed my Diploma in Information Technology, I
-              am eager to apply my classroom knowledge in a professional space.
-              I have a strong passion for development and I am always looking
-              for ways to enhance my skills in my spare time. I strive for
-              excellence when working individually and as part of the team. I
-              believe in innovative ideas that provide simple and practical
-              solutions, with this being said, I am very adaptive to change and
-              can adjust to any position I am put i. I can work well under
-              pressure, and find that most of my prestigious results come from
-              working under pressure.
-            </subtitle1>
-            <br />
-            <br />
-            <br /> WORK EXPERIENCE <br />
-            <br />
-            <subtitle1>
-              SEASONAL SALES ASSISTANT <br /> Typo Vaal Mall <br /> November
-              2020 to February 2021 <br />
-              <br />
-              SEASONAL SALES ASSISTANT <br /> Typo Vaal Mall <br /> November
-              2019 to January 2020 <br />
-            </subtitle1>
-            <br />
-            <br />
-            <br />
-            EDUCATION <br />
-            <br />
-            <subtitle1>
-              DIPLOMA IN INFORMATION TECHNOLOGY <br /> Damelin College <br />
-              February 2018 to November 2020 <br />
-              <br />
-              NATIONAL SENIOR CERTIFICATE <br /> Hoerskool Drie Riviere <br />{' '}
-              January 2012 to November 2016
-            </subtitle1>
-          </Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.contact}>
             <img
               src="https://stackblitz.com/files/react-mykk1t/github/vuyelwaleeto/react-mykk1t/master\IMG-20210620-WA0030.jpg"
               alt="my picture here"
             />
+            VUYELWA MAKABA
             <br />
-            <br /> CONTACT <br />
+            <br />
+            <subtitle> INFORMATION TECHNOLOGY GRADUATE </subtitle>
+          </Paper>
+        </Grid>
+        <Grid item xs={5}>
+          <Paper className={classes.contact}>
+            <br /><p> CONTACT</p> <br />
             <br />
             <subtitle2> HOME ADDRESS </subtitle2>
             <br />
@@ -101,9 +62,52 @@ export default function App() {
             <subtitle1>063 086 2362/067 889 3065</subtitle1> <br />
             <br />
             <subtitle2> EMAIL ADDRESS </subtitle2>
+            <br />
             <subtitle1>lindelwamaka99@gmail.com</subtitle1>
             <br />
-            <br /> SKILLS <br />
+            <br /><p> SKILLS </p><br />
+          </Paper>
+        </Grid>
+        <Grid item xs={7}>
+          <Paper className={classes.profile}>
+            <p1>PROFESSIONAL PROFILE</p1>
+            <br />
+            <br />
+            <subtitle1>
+              Having recently completed my Diploma in Information Technology, I
+              am eager to apply my classroom knowledge in a professional space.
+              I have a strong passion for development and I am always looking
+              for ways to enhance my skills in my spare time. I strive for
+              excellence when working individually and as part of the team. I
+              believe in innovative ideas that provide simple and practical
+              solutions, with this being said, I am very adaptive to change and
+              can adjust to any position I am put in. I can work well under
+              pressure, and find that most of my prestigious results come from
+              working under pressure.
+            </subtitle1>
+            <br />
+            <br />
+            <br /> <p1>WORK EXPERIENCE </p1><br />
+            <br />
+            <subtitle1>
+              SEASONAL SALES ASSISTANT <br /> Typo Vaal Mall <br /> November
+              2020 to February 2021 <br />
+              <br />
+              SEASONAL SALES ASSISTANT <br /> Typo Vaal Mall <br /> November
+              2019 to January 2020 <br />
+            </subtitle1>
+            <br />
+            <br />
+            <br />
+            <p1>EDUCATION </p1><br />
+            <br />
+            <subtitle1>
+              DIPLOMA IN INFORMATION TECHNOLOGY <br /> Damelin College <br />
+              February 2018 to November 2020 <br />
+              <br />
+              NATIONAL SENIOR CERTIFICATE <br /> Hoerskool Drie Riviere <br />{' '}
+              January 2012 to November 2016
+            </subtitle1>
           </Paper>
         </Grid>
       </Grid>
