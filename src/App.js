edbe19522fb768dce9f329b42047ec25a8991590
vuyelwaleeto <lindelwamaka99@gmail.com>
2021-07-08 16:45:28 +0200

    1 file modified, 1 added, 2 deleted

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './style.css';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Paper, CardMedia, Card, CardActionArea } from '@material-ui/core';
-import { Image } from 'C:/Users/Vuyelwa/Pictures/pic1.jpg';
+import { Grid, Paper } from '@material-ui/core';
 
 const useStyles = makeStyles({
   header: {
@@ -15,14 +14,15 @@ const useStyles = makeStyles({
     background: 'white',
     fontSize: 20,
     length: 150,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    padding: 20,
+    color: 'burlywood'
   },
   contact: {
     background: 'blanchedalmond',
-    fontWeight: 'bold'
-  },
-  mypic: {
-    height : 100,
+    fontWeight: 'bold',
+    padding: 20,
+    fontSize: 20
   }
 });
 
@@ -31,15 +31,6 @@ export default function App() {
 
   return (
     <div>
-      <Card>
-      <CardActionArea>
-        <CardMedia
-          className={classes.mypic}
-          image="C:\Users\Vuyelwa\Pictures\pic1.jpg"
-          title="mypicture"
-        />
-        </CardActionArea>
-        </Card>
       <Grid container spacing={3} className={classes.grid}>
         <Grid item xs={12}>
           <Paper className={classes.header}>
@@ -48,18 +39,27 @@ export default function App() {
             <subtitle1> INFORMATION TECHNOLOGY GRADUATE </subtitle1>
           </Paper>
         </Grid>
-        <Grid item xs={7}>
+        <Grid item xs={9}>
           <Paper className={classes.profile}>
             PROFESSIONAL PROFILE
+            <br />
             <br />
             <subtitle1>
               Having recently completed my Diploma in Information Technology, I
               am eager to apply my classroom knowledge in a professional space.
               I have a strong passion for development and I am always looking
-              for ways to enhance my skills in my spare time.
+              for ways to enhance my skills in my spare time. I strive for
+              excellence when working individually and as part of the team. I
+              believe in innovative ideas that provide simple and practical
+              solutions, with this being said, I am very adaptive to change and
+              can adjust to any position I am put i. I can work well under
+              pressure, and find that most of my prestigious results come from
+              working under pressure.
             </subtitle1>
             <br />
+            <br />
             <br /> WORK EXPERIENCE <br />
+            <br />
             <subtitle1>
               SEASONAL SALES ASSISTANT <br /> Typo Vaal Mall <br /> November
               2020 to February 2021 <br />
@@ -67,19 +67,41 @@ export default function App() {
               SEASONAL SALES ASSISTANT <br /> Typo Vaal Mall <br /> November
               2019 to January 2020 <br />
             </subtitle1>
+            <br />
+            <br />
+            <br />
+            EDUCATION <br />
+            <br />
+            <subtitle1>
+              DIPLOMA IN INFORMATION TECHNOLOGY <br /> Damelin College <br />
+              February 2018 to November 2020 <br />
+              <br />
+              NATIONAL SENIOR CERTIFICATE <br /> Hoerskool Drie Riviere <br />{' '}
+              January 2012 to November 2016
+            </subtitle1>
           </Paper>
         </Grid>
-        <Grid item xs={5}>
+        <Grid item xs={3}>
           <Paper className={classes.contact}>
             <img
-              src="C:\Users\Vuyelwa\Pictures\pic1.jpg"
+              src="https://stackblitz.com/files/react-mykk1t/github/vuyelwaleeto/react-mykk1t/master\IMG-20210620-WA0030.jpg"
               alt="my picture here"
             />
+            <br />
             <br /> CONTACT <br />
-            <subtitle2>
+            <br />
+            <subtitle2> HOME ADDRESS </subtitle2>
+            <br />
+            <subtitle1>
               45 Millin Street <br /> Duncanville <br /> Vereeniging <br /> 1939
-              <br /> 063 086 2362/067 889 3065 <br /> lindelwamaka99@gmail.com
-            </subtitle2>
+            </subtitle1>
+            <br />
+            <br />
+            <subtitle2> CELLPHONE </subtitle2> <br />
+            <subtitle1>063 086 2362/067 889 3065</subtitle1> <br />
+            <br />
+            <subtitle2> EMAIL ADDRESS </subtitle2>
+            <subtitle1>lindelwamaka99@gmail.com</subtitle1>
             <br />
             <br /> SKILLS <br />
           </Paper>
